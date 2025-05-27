999306a555ecb0e818b68e6066e59118c7eaf7d0
CelesteBia <124384519+CelesteBia@users.noreply.github.com>
2025-05-27 03:25:11 -0300

    Adição de estado global de ingresso e do backend mockado
    A adição do estado global da aplicação foi feita com React Redux, junto com slice e entityadapter, além de introduzir requisições ao banco de dados temporário através do Async Thunk e manter os dados persistentes.

--- Frontend/cinemanager/src/Pages/Cart.jsx
+++ Frontend/cinemanager/src/Pages/Cart.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Header from '../Components/Header';
 import { removeItem, clearCart } from '../Components/CartSlice';
--- Frontend/cinemanager/src/store.jsx
+++ Frontend/cinemanager/src/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './Components/CartSlice';
+import ingressoReducer from './Slices/IngressoSlice';
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    ingressos: ingressoReducer,
   },
 });
 
